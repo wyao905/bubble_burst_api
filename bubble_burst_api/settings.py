0015ed3af2ad5c4667d463bfe08cb1afecfe1acb
wyao905 <warrenyca905@gmail.com>
2020-11-02 00:38:22 -0500

    added gitignore, removed secret key

--- bubble_burst_api/settings.py
+++ bubble_burst_api/settings.py
@@ -11,6 +11,8 @@ https://docs.djangoproject.com/en/3.1/ref/settings/
 """
 
 from pathlib import Path
+from dotenv import load_dotenv
+load_dotenv()
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -20,7 +22,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'o9u-%b14y3$z_bm@mnr%zd_#-u30$yixe!t69&9noo(3oz49gq'
+SECRET_KEY = str(os.getenv('SECRET_KEY'))
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
